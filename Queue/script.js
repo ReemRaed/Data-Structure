5c346477949c3f00ae024f83fde7b5f7714f9c0b
Reem Raed <rmtaha@iti.gov.eg>
2024-09-04 12:10:23 +0300

    Queue-Implementation

---
+++ Queue/script.js
@@ -0,0 +1,41 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+  dequeue() {
+    const element = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return element;
+  }
+  peek() {
+    return this.items[this.front];
+  }
+  size()
+  {
+    return Object.getOwnPropertyNames(this.items).length;
+  }
+  isEmpty()
+  {
+    return this.rear-this.front===0;
+  }
+}
+
+const queue=new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+
+console.log(queue.size())
